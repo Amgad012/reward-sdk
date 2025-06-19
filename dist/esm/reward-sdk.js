1b8b6d230fadad31ace50a3d56c50c9ee644e3d9
Amgad012 <mgmega012@gmail.com>
2025-06-19 20:28:43 +0300

    i guess it's ready now

--- dist/esm/reward-sdk.js
+++ dist/esm/reward-sdk.js
@@ -628,7 +628,7 @@ var n = lr.exports;
 const Tr = ({ width: c = 18, height: y = 18 }) => /* @__PURE__ */ n.jsx("svg", { width: c, height: y, viewBox: "0 0 24 24", fill: "none", xmlns: "http://www.w3.org/2000/svg", children: /* @__PURE__ */ n.jsx(
   "path",
   {
-    d: "M12 9V14M12 19H12.01M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z",
+    d: "M12 7V14M12 17H12.01M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z",
     stroke: "currentColor",
     strokeWidth: "2",
     strokeLinecap: "round",
